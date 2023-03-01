b0ce426ecfa10d1c0765acaf8ff6eb78bfd82f45
Pablo <jospabcor@hotmail.com>
2023-03-01 10:10:40 -0700

    Delete cats

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const API_URL_RANDOM =
 const API_URL_FAVOURITES =
   "https://api.thecatapi.com/v1/favourites?api_key=live_lNUNX228ceQJK7kA0M6amABtS41dqIQfX11bJK959oHUve40QSewOiZbWbVY3gvy";
 
+const API_URL_FAVOURITES_DELETE = (id) =>
+  `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_lNUNX228ceQJK7kA0M6amABtS41dqIQfX11bJK959oHUve40QSewOiZbWbVY3gvy`;
+
 const spanError = document.getElementById("randomGatosError");
 
 async function loadRandomCats() {
@@ -36,26 +39,36 @@ async function loadRandomCats() {
 async function loadFavouriteCats() {
   const res = await fetch(API_URL_FAVOURITES);
   const data = res.status === 200 ? await res.json() : await res.text();
-
+  const sectionFavs = document.getElementById("favoritesMichis");
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + " " + data.message;
+  } else {
+    const favsArr = [];
+    const container = document.createElement("div");
+    container.classList.add("imgContainerFavs");
+    sectionFavs.innerHTML = "";
+    const h2 = document.createElement("h2");
+    const h2Text = document.createTextNode("Gatitos Favoritos");
+    h2.append(h2Text);
+    h2.classList.add("h2Title");
+    sectionFavs.append(h2);
+    sectionFavs.append(container);
+
+    data.forEach((item) => {
+      const art = document.createElement("article");
+      const image = document.createElement("img");
+      const button = document.createElement("button");
+      const textBtn = document.createTextNode("Quitar de favoritos");
+
+      button.append(textBtn);
+      button.addEventListener("click", () => deleteFavouriteCat(item.id));
+      image.classList.add("catImgg");
+      image.src = item.image.url;
+      art.append(image, button);
+      favsArr.push(art);
+    });
+    container.append(...favsArr);
   }
-
-  const favsArr = [];
-  const container = document.querySelector(".imgContainerFavs");
-  data.forEach((item) => {
-    const art = document.createElement("article");
-    const image = document.createElement("img");
-    const button = document.createElement("button");
-    const textBtn = document.createTextNode("Quitar de favoritos");
-
-    button.append(textBtn);
-    image.classList.add("catImgg");
-    image.src = item.image.url;
-    art.append(image, button);
-    favsArr.push(art);
-  });
-  container.append(...favsArr);
 }
 
 async function saveFavouriteCat(id) {
@@ -68,6 +81,28 @@ async function saveFavouriteCat(id) {
       image_id: id,
     }),
   });
+
+  const data = res.status === 200 ? await res.json() : await res.text();
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + " " + data.message;
+  } else {
+    loadFavouriteCats();
+  }
+}
+
+async function deleteFavouriteCat(id) {
+  const res = await fetch(API_URL_FAVOURITES_DELETE(id), {
+    method: "DELETE",
+  });
+
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + " " + data.message;
+  } else {
+    console.log("gato eliminado de favoritos");
+  }
 }
 
 const btn = document.querySelector(".rdnCat");
